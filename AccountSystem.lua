71f47cd2939ffbed44828e28273445661c99e032
theyfearyin <rythimicwtf@gmail.com>
2026-01-19 16:26:22 -0600

    Create AccountSystem.lua

---
+++ AccountSystem.lua
@@ -0,0 +1,25 @@
+local AccountSystem = {}
+
+AccountSystem.Accounts = {
+    {
+        Username = "aero",
+        Password = "aero123",
+        IsActive = false
+    },
+    {
+        Username = "yin",
+        Password = "longlivejeff",
+        IsActive = true
+    },
+}
+    {
+        Username = "solo",
+        Password = "solovvsx",
+        IsActive = true
+    },
+    {
+        Username = "ylnyang",
+        Password = "RulerofZeeseas",
+        IsActive = true
+    },
+return AccountSystem
